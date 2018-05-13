9782133e6423b2515923ff6f0166199cba65f0de
goldGreatChen <chenjinwei1109@gmail.com>
2018-05-13 20:38:13 +0800

    chushihua

--- src/main/java/com/chen/demo4j/controller/HelloController.java
+++ src/main/java/com/chen/demo4j/controller/HelloController.java
@@ -1,4 +1,21 @@
 package com.chen.demo4j.controller;
 
+import com.chen.framework.annotation.Controller;
+import com.chen.framework.annotation.RequestMapping;
+import com.chen.framework.bean.Data;
+import com.chen.framework.bean.Param;
+import com.chen.framework.bean.Request;
+
+import java.util.HashMap;
+
+@Controller
 public class HelloController {
+
+    @RequestMapping(value = "hello",type = Request.REQUEST_METHOD_GET)
+    public Data getHello(Param param){
+        HashMap<String,String> map=new HashMap<>();
+        map.put("hello","chenjinwei");
+        Data data=new Data(map);
+        return data;
+    }
 }
